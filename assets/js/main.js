5f070ae9963bc6af29594b8f8aef9e9cd40b0c78
Nagaruu <bachduongpmd@gmail.com>
2020-12-12 15:12:25 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+//Owl carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    navigation : true,
+    navText: ["<i class='btnCarousel fa fa-chevron-circle-left' aria-hidden='true'></i>","<i class='btnCarousel fa fa-chevron-circle-right' aria-hidden='true'></i>"],
+
+    responsive:{
+        1920:{
+            items:3
+        },
+        1200:{
+            items:3
+        },
+        1024:{
+            items:2
+        },
+        640:{
+            items:2
+        },
+        480:{
+            items:2
+        },
+        0:{
+            items:1
+        }
+    }
+});
+
+//close button collapse
+$(document).ready(function(){
+    var close = document.getElementById('close');
+    close.addEventListener('click',function(){
+        $('.nav-menu').collapse('hide');
+        // jQuery('#overlay').css('opacity', '0');
+        // jQuery("#overlay").css('visibility', 'hidden');
+    });
+});
+
+//overlay
+$(document).ready(function(){
+    
+    $('#btn').on('click', function(){
+       // $(.nav-menu).css("transform",  "translateX('0')");
+        // jQuery('#overlay').css('visibility', '0.5');
+        // jQuery("#overlay").css('visibility', 'visible');
+    });
+});
