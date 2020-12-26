38ec99adf4ba0afddfa4e6e68c3e0502465c1868
Nagaruu <bachduongpmd@gmail.com>
2020-12-26 17:04:59 +0700

    fix header ,scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,64 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-//close button collapse
+//click button to focus search input
 $(document).ready(function(){
-    var close = document.getElementById('close');
-    close.addEventListener('click',function(){
-        $('.nav-menu').collapse('hide');
-        // jQuery('#overlay').css('opacity', '0');
-        // jQuery("#overlay").css('visibility', 'hidden');
+    var search_in = document.getElementById('search-button');
+    search_in.addEventListener('click',function(){
+        $("#header__search").show();
+        document.getElementById('form-search').focus();
+        document.getElementById('close-button').click();
+        $('.header__top').css('border-bottom', '0');
+        $('.search-navbar').css('border-bottom', 'solid 1px #ddd');
     });
 });
+
+//scroll event
+$(document).ready(function(){
+    var prevScrollpos = window.pageYOffset;
+    
+    window.onscroll = function() {
+        //change position sticky to fixed
+        var currentScrollPos = window.pageYOffset;
+        var topbar = document.querySelector('.topbar');
+        var header = document.querySelector('.header');
+
+        var viewportOffset = topbar.getBoundingClientRect();
+        var top = viewportOffset.top;
+
+        if (top <= -46) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+
+        //scroll up hide header
+        if($(window).width() <  1200)
+        {
+            if (prevScrollpos > currentScrollPos) {
+                console.log(prevScrollpos);
+                if (prevScrollpos < 10) {
+                    $("#header__search").show();
+                    $('.header__top').css('border-bottom', '0');
+                    $('.search-navbar').css('border-bottom', 'solid 1px #ddd');
+                } else {
+                    $('.header__top').css('border-bottom', 'solid 1px #ddd');
+                    $('.search-navbar').css('border-bottom', '0');
+                    $("#header__search").hide();
+                }
+                header.style.top = "0";
+            } else {
+                header.style.top = "-200px";
+            }
+            prevScrollpos = currentScrollPos;
+        } else {
+            if (prevScrollpos > currentScrollPos) {
+                header.style.top = "0";
+            } else {
+                header.style.top = "-200px";
+            }
+            prevScrollpos = currentScrollPos;
+        }
+    }
+});
+
